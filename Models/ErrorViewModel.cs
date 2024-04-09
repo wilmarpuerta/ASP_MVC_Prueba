db791ccbe962e52036d3112e2b95bcdacb35c9cc
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-04-09 14:47:01 -0500

    Inicio de proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ASP_MVC_Prueba.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
